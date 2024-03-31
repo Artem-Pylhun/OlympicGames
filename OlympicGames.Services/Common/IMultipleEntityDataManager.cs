c7b90a4e5467716d572029b0d1605c41f11dbe8c
Maxym Tomusiak <maxtomusiak315@gmail.com>
2024-03-31 19:20:20 +0300

    feat: add data managers interfaces

---
+++ OlympicGames.Services/Common/IMultipleEntityDataManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlympicGames.Services.Common
+{
+    public interface IMultipleEntityDataManager : IFilter
+    {
+        void SetAll<T>(IEnumerable<T> entities);
+        void Read(string path);
+        void Write(string path);
+        IEnumerable<T> GetAll<T>();
+        bool IsChanged { get; }
+        void Add(object entity);
+
+        void Delete(object entity);
+        void Update(object oldEntity, object newEntity);
+    }
+}

51512dfb86a716bc69f7f3dc58cc41bd261abc31
Maxym Tomusiak <maxtomusiak315@gmail.com>
2024-03-31 19:28:49 +0300

    feat: add empty data managers

---
+++ OlympicGames.Services/OlympicDataManagers/XmlOlympicDataManager.cs
@@ -0,0 +1,51 @@
+﻿using OlympicGames.Services.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlympicGames.Services.OlympicDataManagers
+{
+    public class XmlOlympicDataManager : IMultipleEntityDataManager
+    {
+        public bool IsChanged => throw new NotImplementedException();
+
+        public string Filter => throw new NotImplementedException();
+
+        public void Add(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAll<T>(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(object oldEntity, object newEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
